5a5a9886ecfa61c067b4a38eb32d2429ea530189
transon <transon1901@gmail.com>
2023-10-30 20:48:41 +0700

    hello

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const home = require("./routes/home");
-const about = require("./routes/about");
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set("views","./views");
-
 app.use(express.json());
-app.use("/", home);
-app.use("/about", about);
 
+app.use("/", home);
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));

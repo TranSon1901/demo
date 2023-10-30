52434b2de6a66603ab18e24840ca06cbeba625ab
transon <transon1901@gmail.com>
2023-10-30 21:46:53 +0700

    layout

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+/*Import */
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+require('dotenv').config();
 const home = require("./routes/home");
 const about = require("./routes/about");
 
 const app = express();
 app.use(express.json());
+
+/* Set Templating Engine */
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+
+/*Static Files */ 
 app.use(express.static(__dirname + "public"));
 
+/*Layout ejs */
+app.use(expressLayouts);
+app.set('layout', './layouts/layout');
+
+/*Routes */ 
 app.use("/", home);
 app.use("/", about);
 
-
-const port = 9001;
+/*Listen on Port*/
+const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));

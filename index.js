8a767ddec10cf48207398ed80a35e03b3affcc82
transon <transon1901@gmail.com>
2023-10-30 21:04:11 +0700

    render

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const home = require("./routes/home");
 const app = express();
 app.use(express.json());
 app.set('view engine', 'ejs');
+app.set("views","./views");
 
 // app.use("/", home);
 app.get("/", (req, res, next) => {
-  return res.render('home');
+  return res.render("chào", {title:"an", name:"teo"})
+  // return res.render('home');
 });
 
-const port = process.env.PORT || 9001;
+const port = 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));

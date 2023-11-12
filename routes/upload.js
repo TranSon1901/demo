9bb13933b787633a6408ea6afb8a58b6ec7da44c
transon <transon1901@gmail.com>
2023-11-12 21:13:09 +0700

    updateInput

--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const firebsae = require("firebase/app");
-const getAnalytics = require("firebase/analytics");
 const multer = require('multer');
 const { getStorage, ref, getDownloadURL,uploadBytesResumable } = require("firebase/storage");
 const User = require("../models/User");
@@ -17,47 +16,33 @@ const firebaseConfig = {
 };
 
 
-//Initialize a firebase application
+/* upload server 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads")
+    },
+    filename: function (req, file, cb) {
+        const parts = file.mimetype.split("/");
+        cb(null, `${file.fieldname}-${Date.now()}.${parts[1]}`)
+    }
+})
+const upload = multer({ storage: storage }); */
+
+/*upload firebase */
 firebsae.initializeApp(firebaseConfig);
-// getAnalytics.firebsae()
-
-// Initialize Cloud Storage and get a reference to the service
 const storage = getStorage();
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, "uploads")
-//     },
-//     filename: function (req, file, cb) {
-//         const parts = file.mimetype.split("/");
-//         cb(null, `${file.fieldname}-${Date.now()}.${parts[1]}`)
-//     }
-// })
-
-// Setting up multer as a middleware to grab photo uploads
 const upload = multer({ storage: multer.memoryStorage() });
-// const upload = multer({ storage: storage });
 
 router.post("/", upload.single("filename"), async (req, res) =>{
   console.log(req.body);  
   console.log(req.file); 
-//   return res.send("ok");
     try {
         const dateTime = giveCurrentDateTime();
-        const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
-        const metadata = {
-            contentType: req.file.mimetype,
-        };
+        const storageRef = ref(storage, `files/${req.file.originalname + " " + dateTime}`);
+        const metadata = {contentType: req.file.mimetype};
         const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
         const downloadURL = await getDownloadURL(snapshot.ref);
     
-        console.log('File successfully uploaded.');
-        // return res.send({
-        //     message: 'file uploaded to firebase storage',
-        //     name: req.file.originalname,
-        //     type: req.file.mimetype,
-        //     downloadURL: downloadURL
-        // })
         const newUser = User({
             ...req.body,
             img:downloadURL
